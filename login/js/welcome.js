3c3bf35965d850cd8f8f3e0a258adfaddb81c0d3
uniqueusername <hjjyun499@gmail.com>
2026-06-06 20:06:24 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const user = localStorage.getItem("username");
 
     if (user) {
-        document.querySelectorAll("#userInfo").innerText = "Halo, " + user;
+        document.getElementById("userInfo").innerText = "Halo, " + user;
 
-        document.querySelectorAll("#authArea").innerHTML = `
+        document.getElementById("authArea").innerHTML = `
             <button onclick="logout()" class="nav-cta">Logout</button>
         `;
     }
